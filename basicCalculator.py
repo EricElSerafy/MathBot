6bbdde9f0b4e2b4bb909b2c02b7f67fc3ae42cce
Eric <ricofm66@gmail.com>
2021-05-03 14:21:04 -0400

    Added basic calculator to the bot

--- basicCalculator.py
+++ basicCalculator.py
@@ -3,4 +3,14 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix = '?')
 
+@bot.command()
+async def ping(context):
+    await context.send(f'{round(bot.latency*1000)} ms')
+    print('hi')
+
+@bot.command('calc')
+async def calculate(context, *, problem):
+    await context.send(f'Problem: {problem}\nAnswer: {eval(problem)}') 
+    
+    
 bot.run('ODM4NTQ3NjU0OTU0ODQ0MTkw.YI8sXA.UTF8HLgq4bUfaA_-5C4iND9MTuM')
