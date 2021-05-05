d839e573ab70c851ece68a9c6d0ca0728a616df0
Eric El-Serafy <ricofm66@gmail.com>
2021-05-05 12:49:22 -0400

    Changed the exponentiation operator from ** to ^

--- basicCalculator.py
+++ basicCalculator.py
@@ -8,9 +8,17 @@ async def ping(context):
     await context.send(f'{round(bot.latency*1000)} ms')
     print('hi')
 
+# prob = '2^3'
+# prob.replace
+
 @bot.command('calc')
 async def calculate(context, *, problem):
-    await context.send(f'Problem: {problem}\nAnswer: {eval(problem)}') 
+    if '^' in problem:
+        await context.send(f'Problem: {problem}')
+        problem = problem.replace('^', '**')   
+        print(problem)
+        
+        await context.send(f'Answer: {eval(problem)}') 
     
     
 bot.run('ODM4NTQ3NjU0OTU0ODQ0MTkw.YI8sXA.UTF8HLgq4bUfaA_-5C4iND9MTuM')
