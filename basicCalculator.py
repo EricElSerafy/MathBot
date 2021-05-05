69f3566a47a3c14b80da0acbb7f8fba8aa73bc3d
Eric El-Serafy <ricofm66@gmail.com>
2021-05-05 14:54:10 -0400

    Added discord apiKey to .gitignore

--- basicCalculator.py
+++ basicCalculator.py
@@ -1,3 +1,4 @@
+from apiKey import apiKey
 import discord
 from discord.ext import commands
 
@@ -8,17 +9,14 @@ async def ping(context):
     await context.send(f'{round(bot.latency*1000)} ms')
     print('hi')
 
-# prob = '2^3'
-# prob.replace
+
 
 @bot.command('calc')
 async def calculate(context, *, problem):
     if '^' in problem:
         await context.send(f'Problem: {problem}')
-        problem = problem.replace('^', '**')   
-        print(problem)
-        
+        problem = problem.replace('^', '**')     
         await context.send(f'Answer: {eval(problem)}') 
     
-    
-bot.run('ODM4NTQ3NjU0OTU0ODQ0MTkw.YI8sXA.UTF8HLgq4bUfaA_-5C4iND9MTuM')
+
+bot.run(apiKey)
